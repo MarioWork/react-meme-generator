04070b2c93cc0991c29deb45261c21389ed8ecda
MarioWork <mariovieira.work@hotmail.com>
2022-02-21 22:24:09 -0100

    Added feature of generating random image onclick and add text to the iamge

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,15 +1,66 @@
-import React from "react";
+import { useState } from "react";
 import "./Meme.css";
+import { memesData } from "../memeData.js";
 
 const Meme = () => {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  //Get a random meme from memesArray
+  function getRandomMeme(e) {
+    e.preventDefault();
+    const memesArray = memesData[0].data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+
+    const { url } = memesArray[randomNumber];
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      imageUrl: url,
+    }));
+  }
+
+  //Handle the input text change
+  function handleInputChange(e) {
+    const { value, name } = e.target;
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <form className="form">
-        <input type="text" className="form--input" placeholder="Top text" />
-        <input type="text" className="form--input" placeholder="Bottom Text" />
-        <button className="form--button">Get a new Meme Image</button>
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top text"
+          name="topText"
+          onChange={handleInputChange}
+          value={meme.topText}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom Text"
+          name="bottomText"
+          onChange={handleInputChange}
+          value={meme.bottomText}
+        />
+        <button className="form--button" onClick={getRandomMeme}>
+          Get a new Meme Image
+        </button>
       </form>
-      <img src="" alt="meme" className="meme--image" />
+      <div className="meme--container">
+        <img src={meme.imageUrl} className="meme--image" />
+        <h4 className="meme--top--text">{meme.topText}</h4>
+        <h4 className="meme--bottom--text">{meme.bottomText}</h4>
+      </div>
     </main>
   );
 };
